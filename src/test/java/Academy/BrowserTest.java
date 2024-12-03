301be96d7394d2f11f03524e85a96fe88138678d
fpaulgmail <femipaul@gmail.com>
2024-12-03 12:59:07 +0000

    3nd commit

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -8,24 +8,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BrowserTest {
 
-	
-	@Test
-	public void getData()
-	{
-		System.out.println("Hello Guys");
-		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-		WebDriver driver =new ChromeDriver();
-		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
-		String text =driver.findElement(By.cssSelector("h1")).getText();
-		System.out.println(text);
-		Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
-		driver.close();
-	
-		
-		
-	}
+    @Test
+    public void getData() {
+        System.out.println("Hello Guys");
+        
+        // Automatically set up the ChromeDriver using WebDriverManager
+        WebDriverManager.chromedriver().setup();
+        
+        // Now, no need to manually set the system property
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
+        
+        String text = driver.findElement(By.cssSelector("h1")).getText();
+        System.out.println(text);
+        
+        Assert.assertTrue(text.equalsIgnoreCase("RahulShettyAcademy.com Learning"));
+        
+        driver.close();
+    }
 }
+
